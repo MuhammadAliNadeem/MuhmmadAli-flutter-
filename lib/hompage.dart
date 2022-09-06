195e2a2233b000a3ad19f17a1fdc05d0ca18fa5c
MuhammadAliNadeem <an1284005@gmail.com>
2022-09-06 17:26:11 +0500

    day6

--- lib/hompage.dart
+++ lib/hompage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+//import 'package:practice/utils/routes.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -25,6 +26,11 @@ class Homepage extends StatelessWidget {
        // ignore: prefer_const_constructors, dead_code, unused_label
        drawer: Drawer() ;
     
-    
+  
+  // ElevatedButton(onPressed:() {
+  //     Navigator.pushNamed(context, MyRoutes.LoginRoute) ;} 
+  //     // ignore: prefer_const_constructors
+  //     , child: Text("Hello"));
+
   }
 }
